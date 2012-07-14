6e727499cd623739651017d6cb79f4c5e267795a
yokoloko <jeremie.thomassey@gmail.com>
2012-07-14 16:35:23 +0200

    Un peu plus de possibilité :O

--- Appartager.user.js
+++ Appartager.user.js
@@ -5,102 +5,168 @@
 // @include     www.appartager.com
 // @version     1
 // @require     http://code.jquery.com/jquery-1.7.2.min.js
+// @resource    style https://raw.github.com/yokoloko/Appartager-1/master/style.css
 // ==/UserScript==
-//alert('alert1');
 
 
- console.debug('start: add CSS');
-// var cssTxt  = GM_getResourceText("style");
-// GM_addStyle (cssTxt);
- console.debug('done: add CSS');
-
- alert('alert3');
+var cssTxt  = GM_getResourceText("style");
+GM_addStyle (cssTxt);
 
 $(function () {
-	'use strict';
-	var $pages = $('.erm-pages a'), finished = 0, cachedAnnonces = {},
-	    $menu = $('<ul id="papa-annonce-menu"></ul>');
-	alert('alert4');
-	if (GM_getValue('annonces')) {
-		cachedAnnonces = JSON.parse(GM_getValue('annonces'))
-	}
-	alert('alert5');
-	var rewriteCache = function () {
-	    GM_setValue('annonces', JSON.stringify(cachedAnnonces));
-	}
+    'use strict';
+    var $pages = $('.erm-pages a'), finished = 0, cachedAnnonces = {},
+        $container = $('<div id="papa-container"></div>'), $menu = $('<ul id="papa-annonce-menu"></ul>'),
+        $visible = false, className = ' hide';
 
-	var rewriteMenu = function () {
-		$menu.html('');
+    $('.erm-header-navbar-links').find('li:last').after('<li class="erm-float-left erm-navbar-divider"></li><li class="erm-float-left"><a class="erm-white erm-hover" id="papa-launcher" href="#">Simplificateur de recherche.</a></li>');
 
-		$.each(cachedAnnonces, function(key, annonce) {
-			var $li = $('<li></li>');
-			var $link = $('<a data-papa-annonce-id="' + annonce.id + '" href="' + annonce.href + '">' + annonce.id + '</a>');
+    $('.green_upgrade').remove();
 
-			if (annonce.visited === true) {
-				$li.addClass('visited');
-			}
+    $('body').append('<div id="loading-content"></div>');
+    $('body').append($container);
 
-			$menu.append($li.append($link));
-		});
+    if (!GM_getValue('visible') || GM_getValue('visible') !== 'visible') {
+        $container.hide();
+    }
 
-		$('body').append($menu);
-	}
+    if (GM_getValue('annonces')) {
+	cachedAnnonces = JSON.parse(GM_getValue('annonces'))
+    }
 
-	rewriteMenu();
+    var rewriteCache = function () {
+	GM_setValue('annonces', JSON.stringify(cachedAnnonces));
+    }
 
-	$pages.each(function () {
-		var $elt = $(this);
+    var appendAnnonces = function (annonces) {
+        $.each(annonces, function() {
+            var annonces = $(this);
 
-		$.get($elt.attr('href'), function (html) {
-			// Traite le resultat de la page ouverte
-			var $listings = $(html).find('.erm-listings .erm-listing');
+	    $.each(annonces, function(key, annonce) {
+	        var $li = $('<li></li>');
+	        var $link = $('<a class="erm-white erm-hover" data-papa-annonce-id="' + annonce.id + '" href="' + annonce.href + '">' + annonce.title + '</a>');
 
-			$listings.each(function () {
-				// Pour chaque utilisateur
-				var $elt = $(this), annonce = {}, id;
+	        if (annonce.visited === true) {
+		    $li.addClass('visited');
+	        }
 
-				$elt.find('a').each(function () {
-					// pour chaque lien trouvé
-					var href = $(this).attr('href');
+	        $menu.append($li.append($link));
+	    });
+        });
+    };
 
-					if (typeof href === 'undefined') {
-						return;
-					}
-					var tmp = href.match(/\/content\/common\/listingdetail.aspx\?code=(.*)&from=.*/);
+    var rewriteMenu = function () {
+        var visited = {}, news = {};
+	$menu.html('');
 
-					if (tmp) {
-						id = tmp[1];
+        $.each(cachedAnnonces, function(key, annonce) {
+            if (annonce.visited) {
+                visited[annonce.id] = annonce;
+            } else {
+                news[annonce.id] = annonce;
+            }
+        });
 
-						if (typeof cachedAnnonces[id] === 'undefined') {
-							annonce.id      = id;
-							annonce.href    = $(this).attr('href');
-							annonce.visited = false;
+        appendAnnonces(news);
+        appendAnnonces(visited);
 
-							cachedAnnonces[id] = annonce;
-						}
-					}
-				});
-			});
+	$container.append($menu);
+    }
 
-			finished += 1;
-		});
-	});
+    rewriteMenu();
 
-	var timer = window.setInterval(function() {
-		if ($pages.length > 0 && finished === $pages.length) {
-			rewriteCache();
-			rewriteMenu();
+    var refreshCache = function () {
+        var found = 0;
+        $pages.each(function () {
+	    var $elt = $(this);
 
-			clearTimeout(timer);
-		}
-	}, 2000);
+	    $.get($elt.attr('href'), function (html) {
+	        // Traite le resultat de la page ouverte
+	        var $listings = $(html).find('.erm-listings .erm-listing');
 
-	$('a[data-papa-annonce-id]').live('click', function(event) {
-		var $elt = $(this), ref = $elt.data('papa-annonce-id');
+	        $listings.each(function () {
+		    // Pour chaque utilisateur
+		    var $elt = $(this), annonce = {}, id;
 
-		cachedAnnonces[ref].visited = true;
+		    $elt.find('a').each(function () {
+		        // pour chaque lien trouvé
+		        var href = $(this).attr('href');
 
-		rewriteCache();
-	});
+		        if (typeof href === 'undefined') {
+			    return;
+		        }
+		        var tmp = href.match(/\/content\/common\/listingdetail.aspx\?code=(.*)&from=.*/);
 
+		        if (tmp) {
+			    id = tmp[1];
+
+			    if (typeof cachedAnnonces[id] === 'undefined') {
+                                found ++;
+			        annonce.id      = id;
+			        annonce.href    = $(this).attr('href');
+			        annonce.visited = false;
+
+                                annonce.title   = $elt.find('.erm-listing-details strong').text();
+
+			        cachedAnnonces[id] = annonce;
+			    }
+
+                            return false;
+		        }
+		    });
+	        });
+
+	        finished += 1;
+	    });
+        });
+
+        var timer = window.setInterval(function() {
+	    if ($pages.length > 0 && finished === $pages.length) {
+                alert(found + ' utilisateurs trouves.');
+
+	        rewriteCache();
+	        rewriteMenu();
+
+	        clearTimeout(timer);
+	    }
+        }, 2000);
+    }
+
+    $('a[data-papa-annonce-id]').live('click', function(event) {
+	var $elt = $(this), ref = $elt.data('papa-annonce-id');
+
+	cachedAnnonces[ref].visited = true;
+
+	rewriteCache();
+    });
+
+    if ($pages.length > 0) {
+        $container.prepend('<button id="papa-refresh" type="submit">Refresh cache</button>');
+
+        $('#papa-refresh').live('click', function(event) {
+            refreshCache();
+        });
+    } else {
+        $container.prepend('<strong>Vous devez etre sur la page de recherche pour rafraichir le cache.</strong>');
+    }
+
+
+    $('#loading-content')
+        .ajaxStart(function() {
+            var msg = $('<div id="infscr-loading"><img alt="Loading..." src="http://i.imgur.com/6RMhx.gif" /><div>chargement</div></div>');
+
+            $('#loading-content').html(msg).show();
+        })
+        .ajaxStop(function() {
+            $('#loading-content').hide();
+        });
+
+    $('#papa-launcher').click(function() {
+        $container.toggle();
+
+        if ($container.is(':visible')) {
+            GM_setValue('visible', 'visible');
+        } else {
+            GM_setValue('visible', 'hide');
+        }
+    });
 });
